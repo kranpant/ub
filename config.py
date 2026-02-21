31016380e37e06073aa33fead219b51875962a88
kranpant <110833432+kranpant@users.noreply.github.com>
2026-02-21 21:01:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,40 +2,47 @@ from os import getenv, path
 from dotenv import load_dotenv
 from data import THE_SHUKLA
 
-
 if path.exists(".env"):
     load_dotenv(".env")
-    
-#----------------------------------- REQUIRED --------------------------------------#
 
+# ------------------------------ REQUIRED ------------------------------ #
 
-API_ID = int(getenv("API_ID", None))
-API_HASH = getenv("API_HASH", None)
-SESSION1 = getenv("SESSION1", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = list(map(int, getenv("OWNER_ID", "6762113050").split()))
+API_ID = int(getenv("API_ID", "0"))
+API_HASH = getenv("API_HASH", "")
+SESSION1 = getenv("SESSION1", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 
+OWNER_ID = list(
+    map(int, getenv("OWNER_ID", "6762113050").split())
+)
 
-#-------------------------------- OPTIONAL -------------------------------------#
+# ------------------------------ OPTIONAL ------------------------------ #
 
-SESSION2 = getenv("SESSION2")
-SESSION3 = getenv("SESSION3")
-SESSION4 = getenv("SESSION4")
-SESSION5 = getenv("SESSION5")
+SESSION2 = getenv("SESSION2", "")
+SESSION3 = getenv("SESSION3", "")
+SESSION4 = getenv("SESSION4", "")
+SESSION5 = getenv("SESSION5", "")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itzshukla/STRANGER-OPUSERBOT")
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/itzshukla/STRANGER-OPUSERBOT"
+)
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
-GIT_TOKEN = getenv("GIT_TOKEN", None)
+GIT_TOKEN = getenv("GIT_TOKEN", "")
 
-EXTRA_IMG = getenv("EXTRA_IMG", "https://files.catbox.moe/uufiry.jpg")
+EXTRA_IMG = getenv(
+    "EXTRA_IMG",
+    "https://files.catbox.moe/uufiry.jpg"
+)
 
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", "")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", "")
 
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6919199044").split()))
+SUDO_USERS = list(
+    map(int, getenv("SUDO_USERS", "6919199044").split())
+)
 
 for y in OWNER_ID:
     SUDO_USERS.append(y)
@@ -44,7 +51,5 @@ for x in THE_SHUKLA:
     SUDO_USERS.append(x)
 
 LOAD = []
-
 NO_LOAD = []
-
 HELPABLE = {}
