7c3830e522b7611ffdb901e09473e33711aef807
kranpant <110833432+kranpant@users.noreply.github.com>
2026-03-12 08:50:01 +0530

    Update __init__.py

--- SHUKLAUSER/__init__.py
+++ SHUKLAUSER/__init__.py
@@ -1,28 +1,24 @@
 import time
-import asyncio
 from aiohttp import ClientSession
-from SHUKLAUSER.Helpers import app, github
+from SHUKLAUSER.Helpers import app, github, one, two, three, four, five
+
 
 __Version__ = "1.0.0"
 
 boot = time.time()
 
 app = app
+
 one = one
+
 two = two
+
 three = three
+
 four = four
+
 five = five
 
 github()
 
-# Create a placeholder session
-aiohttpsession = None
-
-# Function to initialize it later
-async def get_session():
-    global aiohttpsession
-    if aiohttpsession is None or aiohttpsession.closed:
-        aiohttpsession = ClientSession()
-    return aiohttpsession
-  
+aiohttpsession = ClientSession()
